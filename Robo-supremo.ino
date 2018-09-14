108bb9bbdf5125db9f338c6afb748bde9f0d6503
yago Aparecido dos Santos Silva <43183773+YagogesKRYEG@users.noreply.github.com>
2018-09-14 16:25:30 -0300

    Add files via upload

---
+++ Robo-supremo.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  circulo();
+ 
+}
+
+void circulo() {
+  analogWrite(5, 80);
+  digitalWrite(6, HIGH);
+  analogWrite(9, 80);
+  digitalWrite(10, LOW);
+  
+  }
